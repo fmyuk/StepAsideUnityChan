301b0986fc768d20f84a9a0169d63e7bbb290031
fumi uk <dcfumiya.238@gmail.com>
2019-02-24 16:42:18 +0900

    Unity-Chan通過後のGameObjectの破棄
    大きさを調節したCubeをMainCameraに取り付けてCubeControllerを取り付けると、Unity-Chan通過後、GameObjectが破棄されるようになりました。

---
+++ Assets/CubeController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeController : MonoBehaviour {
+
+
+
+    private Rigidbody rgb;
+
+    // Use this for initialization
+    void Start () {
+        this.rgb = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnTriggerEnter(Collider otherObj)
+    {
+        if (otherObj.gameObject.tag == "TrafficConeTag" || otherObj.gameObject.tag == "CarTag" || otherObj.gameObject.tag == "CoinTag")
+        {
+            Destroy(otherObj.gameObject);
+        }
+    }
+}
